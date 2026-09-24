b52134dab9a0de8c97d37d6392a23a73d07ae697
Kufik <127012125+Onmaynec@users.noreply.github.com>
2026-09-24 17:05:17 +0500

    Добавить packages/core/src/index.ts

---
+++ packages/core/src/index.ts
@@ -0,0 +1,13 @@
+export {
+  MODULES,
+  SETTINGS_CATEGORIES,
+  getCategory,
+  getModule,
+  getModulesByCategory
+} from "./modules.js";
+
+export type {
+  ModuleDefinition,
+  ModuleKey,
+  SettingsCategoryKey
+} from "./modules.js";
